fix bug in main.py, avoid nan gradients, add TensorUtils.py

main.py
@@ -1,12 +1,9 @@
-from noot.models.CenterNetLite import CenterNetLite
+from noot.models.CenterNet import CenterNet
 from noot.preprocess.Preprocess import Preprocess
 from time import time
-import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import seaborn as sns
 
 try:
   physical_devices = tf.config.list_physical_devices('GPU')
@@ -22,11 +19,11 @@ except:
   dataset = dataset['train'].map(Preprocess.flatten_dictionary).filter(Preprocess.filter_nonperson).map(Preprocess.exclude_nonperson_label).map(Preprocess.normalize_image).map(Preprocess.rescale_image)
   tf.data.experimental.save(dataset, 'datasets/coco')
 
-model = CenterNetLite()
+model = CenterNet()
 model.compile(run_eagerly=True)
-model.optimizer = tf.keras.optimizers.Adam(learning_rate=2.5e-4)
+model.optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 
-batch_size = 16
+batch_size = 5
 val_images = 1024
 max_n_epochs = 256
 best_loss = 10000

noot/models/CenterNet.py
@@ -1,3 +1,4 @@
+from noot.utils.TensorUtils import TensorUtils
 import tensorflow as tf
 
 class CenterNet(tf.keras.Model):
@@ -49,8 +50,9 @@ class CenterNet(tf.keras.Model):
       loss, keypoint_loss, offset_loss, size_loss = self.compute_loss(data, outputs, lambda_offset=lambda_offset, lambda_size=lambda_size)
     
     gradients = tape.gradient(loss, self.trainable_variables)
+    gradients = TensorUtils.remove_nan_gradients(gradients)
     self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-    
+
     return { 'loss': loss.numpy(), 'keypoint_loss': keypoint_loss.numpy(), 'offset_loss': offset_loss.numpy(), 'size_loss': size_loss.numpy() }
 
   def compute_loss(self, inputs, outputs, alpha=2, beta=4, lambda_offset=1, lambda_size=0.2):
@@ -82,7 +84,8 @@ class CenterNet(tf.keras.Model):
 
     pi = 3.1415926
     radius = tf.math.sqrt(0.3 * area / pi)
-    p_hat_sigma = tf.reshape(2 / 3 * radius * tf.ones((n, max_n_keypoints)), (n, max_n_keypoints, 1, 1))
+    p_hat_sigma = tf.reshape(
+        2 / (3 * radius * tf.ones((n, max_n_keypoints)) + 1e-3), (n, max_n_keypoints, 1, 1))
 
     coord_x = tf.zeros((lowres_height, lowres_width)) + tf.cast(tf.range(lowres_width), tf.float32)
     coord_y = tf.transpose(tf.zeros((lowres_width, lowres_height)) + tf.cast(tf.range(lowres_height), tf.float32), [1, 0])
@@ -94,7 +97,7 @@ class CenterNet(tf.keras.Model):
     coord_y = tf.reshape(coord_y, (n, 1, lowres_height, lowres_width))
 
     y_pred = outputs[:, :, :, 0]
-    y_true = tf.math.exp(-1 * ((coord_x-p_hat_x_tensor)** 2 + (coord_y-p_hat_y_tensor)**2)/p_hat_sigma)
+    y_true = tf.math.exp(-1 * ((coord_x-p_hat_x_tensor)** 2 + (coord_y-p_hat_y_tensor)**2) / p_hat_sigma)
     y_true = tf.reduce_max(y_true, axis=1)
 
     keypoint_loss = -1 / n_keypoints * tf.reduce_sum(

noot/preprocess/Preprocess.py
@@ -55,25 +55,27 @@ class Preprocess:
     scaled_height: int = 512,
   ):
     scaled_image = tf.image.resize(X['image'], (scaled_height, scaled_width))
-    n_objects = len(X['objects/label'])
-    n_padding = max_n_objects - n_objects
-    if n_padding >= 0:
-      area = tf.map_fn(fn=lambda x: scaled_width * (x[3] - x[1]) * scaled_height * (x[2] - x[0]), elems=X['objects/bbox'])
-      area = tf.pad(area, tf.convert_to_tensor([[0, n_padding]]), 'CONSTANT', constant_values=-1.0)
-      label = tf.pad(X['objects/label'], tf.convert_to_tensor([[0, n_padding]]), 'CONSTANT', constant_values=-1)
-      bbox = tf.pad(X['objects/bbox'], tf.convert_to_tensor([[0, n_padding], [0, 0]]), 'CONSTANT', constant_values=-1.0)
-    else:
-      area = tf.cast(X['objects/area'][0:max_n_objects], tf.float32)
-      label = X['objects/label'][0:max_n_objects]
-      bbox = X['objects/bbox'][0:max_n_objects]
+    if 'objects/label' in X:
+      n_objects = len(X['objects/label'])
+      n_padding = max_n_objects - n_objects
+      if n_padding >= 0:
+        area = tf.map_fn(fn=lambda x: scaled_width * (x[3] - x[1]) * scaled_height * (x[2] - x[0]), elems=X['objects/bbox'])
+        area = tf.pad(area, tf.convert_to_tensor([[0, n_padding]]), 'CONSTANT', constant_values=-1.0)
+        label = tf.pad(X['objects/label'], tf.convert_to_tensor([[0, n_padding]]), 'CONSTANT', constant_values=-1)
+        bbox = tf.pad(X['objects/bbox'], tf.convert_to_tensor([[0, n_padding], [0, 0]]), 'CONSTANT', constant_values=-1.0)
+      else:
+        area = tf.cast(X['objects/area'][0:max_n_objects], tf.float32)
+        label = X['objects/label'][0:max_n_objects]
+        bbox = X['objects/bbox'][0:max_n_objects]
 
     X['image'] = scaled_image
-    X['objects/area'] = area
-    X['objects/label'] = label
-    X['objects/bbox'] = bbox
-    del X['image/filename']
-    del X['image/id']
-    del X['objects/id']
-    del X['objects/is_crowd']
+    if 'objects/label' in X:
+      X['objects/area'] = area
+      X['objects/label'] = label
+      X['objects/bbox'] = bbox
+      del X['image/filename']
+      del X['image/id']
+      del X['objects/id']
+      del X['objects/is_crowd']
   
     return X